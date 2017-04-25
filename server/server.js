45e86b49c4810793602cc32b8a585925a22d2870
Jose Starsky Naya Lara <starsky@Joses-MacBook-Pro.local>
2017-04-25 16:42:56 +0200

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -18,14 +18,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connection');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
-        //lisener
         console.log('createMessage', message);
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+
     });
 
     socket.on('disconnect', () => {
